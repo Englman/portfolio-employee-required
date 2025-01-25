09188350792105cdc735363872eacaf9aa8a54be
Englman <17042005v@gmail.com>
2025-01-25 21:29:03 +0500

    Add files via upload

---
+++ frontendSelection/js/index.js
@@ -0,0 +1,6 @@
+var timeInput = document.getElementsByClassName("fcl__timestamp")[0];
+
+var currentYear = new Date().getFullYear();
+
+
+timeInput.innerHTML = `&#169; ${currentYear}, Vijay Singh Design. All Rights Reserved`;
